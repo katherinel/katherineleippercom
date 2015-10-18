4b793e0e964e8cc213be118f42e9d31c862fb637
Katherine Leipper <kleipper@gmail.com>
2015-10-17 21:52:16 -0700

    added some validations to the models

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,5 @@
 class Image < ActiveRecord::Base
 	belongs_to :project
+	validates_presence_of :project
+	validates :url, :presence => true
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ActiveRecord::Base
 	has_many :images
+	validates_presence_of :images
+	validates_associated :images
+	validates :title, :begin_date, :short_description, :long_description, :presence => true
 end
