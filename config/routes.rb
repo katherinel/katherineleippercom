eda5a252b02bd8eb0d705fdf22a726f405cc8f7a
Katherine Leipper <kleipper@gmail.com>
2015-10-27 16:33:39 -0700

    Homepage view and controller; controller for projects; all the sass and coffeescript that comes with it

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+
+  root 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -13,6 +17,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
+  resources :projects
 
   # Example resource route with options:
   #   resources :products do
