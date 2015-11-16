fc3a1507dafd684a70a42837bea4b68f1fc5e90a
Katherine Leipper <kleipper@gmail.com>
2015-11-16 13:32:30 -0800

    started building out projects page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,15 @@
 class ProjectsController < ApplicationController
+	http_basic_authenticate_with name: "admin", password: ENV["SITE_PASSWORD"], only: [:new, :edit, :destroy]
+
 	def index
 		@projects = Project.all
+		@categories = Category.all
 	end
 	
 	def show
 		@project = Project.find(params[:id])
+	end
+
+	def edit
 	end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,16 @@ class Project < ActiveRecord::Base
 	validates_presence_of :images
 	validates_associated :images
 	validates :title, :begin_date, :short_description, :long_description, :presence => true
+
+	def begin_year
+		if self.begin_date != nil
+			self.begin_date.strftime('%Y')
+		end
+	end
+
+	def end_year
+		if self.end_date != nil
+			self.end_date.strftime('%Y')
+		end
+	end
 end
