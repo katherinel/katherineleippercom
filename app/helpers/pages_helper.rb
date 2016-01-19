1d1da9aaf74f857006fa3390730bbd8f11cbe820
Katherine Leipper <kleipper@gmail.com>
2016-01-19 14:37:18 -0800

    project boxes styles in featured section

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,13 @@
 module PagesHelper
+	def proj_title_bkgd(p)
+		if p.image_matching_color?
+			hex = p.image_matching_color
+			length = hex.length # figure out if it's a 6-char or 3-char color
+			rgb = hex.scan(/.{1,#{length/3}}/).map! {|h| h.hex} # convert to rgb in singles or pairs
+			rgb_string = rgb.join(", ")
+			"background-color: rgba(#{rgb_string}, 0.7);"
+		else
+			"background-color: rgba(0, 0, 0, 0.7);"
+		end
+	end
 end
