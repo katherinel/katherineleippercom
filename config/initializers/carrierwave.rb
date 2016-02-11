24acaf963a1321355fe2be0f1c2371283c96aa2e
Katherine Leipper <kleipper@gmail.com>
2016-02-10 21:58:53 -0800

    changed storage to fog and fixed s3 bucket settings

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,7 +14,7 @@ CarrierWave.configure do |config|
       :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
       :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                     # required
       #:region                 => 'us-west-1',                  # optional, defaults to 'us-east-1'
-      :host                   => ' kat-assets.s3-website-us-east-1.amazonaws.com'             # optional, defaults to nil
+      :host                   => 's3-website-us-east-1.amazonaws.com'             # optional, defaults to nil
       #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
     }
     config.cache_dir        = "#{Rails.root}/tmp/uploads"
