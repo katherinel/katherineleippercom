f5364f81a539499c8c3ebbdb89057bd1d1ec4318
Katherine Leipper <kleipper@gmail.com>
2016-02-10 21:42:49 -0800

    added host...

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,14 +12,15 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider               => 'AWS',                        # required
       :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
-      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']                     # required
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                     # required
       #:region                 => 'us-west-1',                  # optional, defaults to 'us-east-1'
-      #:host                   => 's3.example.com',             # optional, defaults to nil
+      :host                   => ' kat-assets.s3-website-us-east-1.amazonaws.com'             # optional, defaults to nil
       #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
     }
     config.cache_dir        = "#{Rails.root}/tmp/uploads"
     config.fog_directory  = ENV['S3_BUCKET_NAME']                             # required
     config.fog_public     = false                                   # optional, defaults to true
+    #config.fog_host       = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
     #config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
   end
 end
