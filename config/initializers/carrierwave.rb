6dfa6c83424a20b7f5593992f3dff4cbe811e263
Katherine Leipper <kleipper@gmail.com>
2016-02-12 23:52:19 -0800

    path_style in effort to get rid of timeout error...

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,6 +16,7 @@ CarrierWave.configure do |config|
       #:region                 => 'us-west-1',                  # optional, defaults to 'us-east-1'
       :host                   => 's3-website-us-east-1.amazonaws.com'             # optional, defaults to nil
       #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+      :path_style =>          true
     }
     config.cache_dir        = "#{Rails.root}/tmp/uploads"
     config.fog_directory  = ENV['S3_BUCKET_NAME']                             # required
