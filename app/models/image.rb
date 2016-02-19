d4396fbdcfd377874ae7fe37b28d3334146aa5de
Katherine Leipper <kleipper@gmail.com>
2016-02-18 22:59:35 -0800

    manually trigger different versions to save

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,10 +1,14 @@
 class Image < ActiveRecord::Base
 	mount_uploader :image, ProjectImageUploader
 	belongs_to :project
-	#validates :image, :presence => true
+	after_save :save_versions
 
 	def image_name
 	  File.basename(image.path || image.filename) if image 
+	end
+
+	def save_versions # don't know why it's not doing this in the first place!
+		self.image.recreate_versions!
 	end
 
 end
