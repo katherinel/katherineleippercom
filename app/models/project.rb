43919bcda10126c5fc6ebc4a41821ec9eb1b7920
Katherine Leipper <kleipper@gmail.com>
2016-02-23 19:03:53 -0800

    take out old validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,22 +7,12 @@ class Project < ActiveRecord::Base
 	accepts_nested_attributes_for :images, allow_destroy: true
 	#validates_presence_of :images
 	validates_associated :images
-	validates :title, :begin_date, :short_description, :long_description, :presence => true
+	validates :title, :begin_date, :short_description, :long_description, :default_image_id, :presence => true
 	validates_format_of :image_matching_color, :with => /[A-Fa-f0-9]{6}|[A-Fa-f0-9]{3}/
-	#validate :must_check_default_image
-	validate :only_one_default_image
 	validate :must_have_category
 
 	def self.featured
 		Project.order("end_date DESC").first(3) # fix this later
-	end
-
-	def must_check_default_image
-		errors.add(:base, "You must select a default image") if self.images.all?{ |i| i.is_default.blank? }
-	end
-
-	def only_one_default_image
-		errors.add(:base, "You can only have one default image") if self.images.where(is_default: true).length > 1
 	end
 
 	def must_have_category
@@ -41,13 +31,5 @@ class Project < ActiveRecord::Base
 		else
 			[begin_year, end_year]
 		end
-	end
-
-	def default_image
-		self.images.find_by(is_default: true)
-	end
-
-	def non_default_images
-		self.images.where(is_default: nil || false)
 	end
 end
