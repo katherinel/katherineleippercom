a010d549b8425bfdc799b9def64618d78f8ad4e9
Katherine Leipper <kleipper@gmail.com>
2016-05-05 18:11:27 -0700

    create contacts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223212818) do
+ActiveRecord::Schema.define(version: 20160506011007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20160223212818) do
   create_table "categories_projects", id: false, force: :cascade do |t|
     t.integer "project_id",  null: false
     t.integer "category_id", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string  "name"
+    t.string  "email"
+    t.integer "phone"
+    t.string  "met"
   end
 
   create_table "images", force: :cascade do |t|
