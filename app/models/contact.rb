421934626a3369e1aabf40396d147c8e7b8763cb
Katherine Leipper <kleipper@gmail.com>
2016-11-06 19:38:36 -0800

    Take off maximum digits for international numbers

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,5 +2,5 @@ class Contact < ActiveRecord::Base
 	validates :name, :met, presence: true
 	validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, :allow_blank => true
 	validates_numericality_of :phone, :only_integer => true, :allow_blank => true
-	validates_length_of :phone, :minimum => 10, :maximum => 10, :message => 'must be ten digits', :allow_blank => true
+	validates_length_of :phone, :minimum => 10, :message => 'must be at least ten digits', :allow_blank => true
 end
